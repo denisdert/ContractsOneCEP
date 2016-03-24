dc1fd16a8e325acdd05edf97f4f8cd71b481df5e
Вотяков Денис Александрович <vda@europlan.ru>
2016-03-24 15:37:46 +0300

    Добавлены параметры описывающие запуск 1с

--- onecscript/contracts.ts
+++ onecscript/contracts.ts
@@ -1,5 +1,10 @@
 import {Integer, Guid, InfoBase} from "../core";
 
+export enum RunningModes{
+	Enterprise//Режим запуска в режиме 1С:Предприятия
+,	Designer//Режим запуска в режиме Конфигуратор
+}//Режимы запуска 1С
+
 export interface InfobaseId extends String {}
 
 export interface InfobaseConfiguration extends InfoBase
@@ -12,10 +17,11 @@ export interface InfobaseConfiguration extends InfoBase
 export interface ScheduleTask{
 	
 	id: String;//Идентификатор регламентного задания
+	description?: String//Описание регламентного задания
 	infobaseId: InfobaseId;//Идентификатор информационной базы на которой запускается задание
-	
 	param: String;//C <строка текста> — передача параметра в конфигурацию. 
-
+	isActive: boolean;//Активно ли задание в текущий момент
+	runningMode: RunningModes;//Режим запуска 1с предприятия
 }
 
 
